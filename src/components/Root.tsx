3a4892580c3b375cd89038a3e54eae07915728ef
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-04-24 13:48:44 +0900

    docs: Add Login Modal
     로그인 모달 추가

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Box, Button, HStack } from '@chakra-ui/react';
-import { FaAirbnb } from 'react-icons/fa';
+import { Box, Button, HStack, IconButton, useDisclosure } from '@chakra-ui/react';
+import { FaAirbnb, FaMoon, FaSun } from 'react-icons/fa';
 import { Outlet } from 'react-router-dom';
+import LoginModal from './LoginModal';
 
 export default function Root() {
+  const {isOpen, onOpen, onClose} = useDisclosure();
   return (
     <Box>
       <HStack justifyContent={"space-between"} px={5} py={10} borderBottomWidth={1}>
@@ -11,9 +13,11 @@ export default function Root() {
           <FaAirbnb size={"38"} />
         </Box>
         <HStack spacing={2}>
-          <Button>Log in</Button>
+          <IconButton variant={"ghost"} aria-label='Toggle dark mode ' icon={<FaMoon />} />
+          <Button onClick={onOpen}>Log in</Button>
           <Button colorScheme={"red"}>Sign up</Button>
         </HStack>
+        <LoginModal isOpen={isOpen} onClose={onClose} />
       </HStack>
     <Outlet />
     </Box>
