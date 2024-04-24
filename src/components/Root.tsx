7d742e902fced4e8de2cb804640decd062ab987a
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-04-24 11:09:23 +0900

    docs: Update root.tsx
     헤더 작성

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Box, Button, HStack } from '@chakra-ui/react';
+import { FaAirbnb } from 'react-icons/fa';
 import { Outlet } from 'react-router-dom';
 
 export default function Root() {
   return (
-    <div>
-        Root
-        <Outlet />
-    </div>
+    <Box>
+      <HStack justifyContent={"space-between"} px={5} py={10} borderBottomWidth={1}>
+        <Box color="red.500">
+          <FaAirbnb size={"38"} />
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+      </HStack>
+    <Outlet />
+    </Box>
   )
 }
