5089edb449b50e9821cf3733092fc6faad8848e1
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-04-25 18:25:42 +0900

    docs: Update Home.tsx
     fetch부를 axios 방식과 react-query로 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import router from "./router";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const client = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={client}>
+            <ChakraProvider theme={theme}>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                <RouterProvider router={router} />
+            </ChakraProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,67 +1,67 @@
-import React from 'react';
-import { Grid } from '@chakra-ui/react';
-import RoomSkeleton from '../components/RoomSkeleton';
-import Room from '../components/Room';
+import React from "react";
+import { Grid } from "@chakra-ui/react";
+import RoomSkeleton from "../components/RoomSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import Room from "../components/Room";
+import { getRooms } from "../api";
 
 interface IPhotos {
-  pk: number;
-  file: string;
-  description: string;
+    pk: number;
+    file: string;
+    description: string;
 }
 interface IRoom {
-  pk: number;
-  name: string;
-  country: string;
-  city: string;
-  price: number;
-  rating: number;
-  is_owner: boolean;
-  photos: IPhotos[];
+    pk: number;
+    name: string;
+    country: string;
+    city: string;
+    price: number;
+    rating: number;
+    is_owner: boolean;
+    photos: IPhotos[];
 }
 
 export default function Home() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [rooms, setRooms] = React.useState<IRoom[]>([]);
-  const fetchRooms = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
-    const json = await response.json();
-    setRooms(json);
-    setIsLoading(false);
-  };
-  React.useEffect(() => {
-    fetchRooms();
-  }, []);
-  return (
-    <Grid mt={10} px={{
-      base: 10,
-      md: 20,
-      lg: 40,
-    }} rowGap={8} columnGap={4} templateColumns={{
-      sm : "1fr",
-      md : "repeat(2, 1fr)",
-      lg : "repeat(3, 1fr)",
-      xl : "repeat(4, 1fr)",
-      "2xl" : "repeat(5, 1fr)" // 숫자를 넣으려면 string형으로 넣어야 함
-    }}>
-      {/* {[1, 2, 3, 4, 5].map(index => (
+    const { isLoading, data } = useQuery<IRoom[]>({
+        queryKey: ["rooms"],
+        queryFn: getRooms,
+    });
+    return (
+        <Grid
+            mt={10}
+            px={{
+                base: 10,
+                md: 20,
+                lg: 40,
+            }}
+            rowGap={8}
+            columnGap={4}
+            templateColumns={{
+                sm: "1fr",
+                md: "repeat(2, 1fr)",
+                lg: "repeat(3, 1fr)",
+                xl: "repeat(4, 1fr)",
+                "2xl": "repeat(5, 1fr)", // 숫자를 넣으려면 string형으로 넣어야 함
+            }}
+        >
+            {/* {[1, 2, 3, 4, 5].map(index => (
         <Room key={index} />
       ))} */}
-      { 
-        isLoading ? 
-        <>
-          <RoomSkeleton />
-        </> : 
-        rooms.map(
-          room => <Room
-                      imageUrl={room.photos[0].file}
-                      name = {room.name}
-                      rating = {room.rating}
-                      city = {room.city}
-                      country = {room.country}
-                      price = {room.price}
-                  />
-        ) 
-      }
-    </Grid>
-  )
+            {isLoading ? (
+                <>
+                    <RoomSkeleton />
+                </>
+            ) : null}
+            {data?.map((room) => (
+                <Room
+                    imageUrl={room.photos[0].file}
+                    name={room.name}
+                    rating={room.rating}
+                    city={room.city}
+                    country={room.country}
+                    price={room.price}
+                />
+            ))}
+        </Grid>
+    );
 }
